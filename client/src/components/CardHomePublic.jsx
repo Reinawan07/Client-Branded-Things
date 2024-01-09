4a4dcc16e12a298726707cd3215d8966a868d1fa
Reinawan <admin@reinawan.com>
2024-01-09 20:44:38 +0700

    feat: Card Home Public

---
+++ client/src/components/CardHomePublic.jsx
@@ -0,0 +1,26 @@
+
+export default function CardHomePublic() {
+
+    return (
+        <>
+            <div className="grid grid-cols-3 gap-4 mb-4">
+                <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div style={{ height: "300px" }}>
+                        <img className="object-cover w-full h-full rounded-t-lg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLF8Bwc6vlme8LCvoO2m0mjUL9dpJ2Gdjn6Q&usqp=CAU" alt="" />
+                    </div>
+
+                    <div className="p-5">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Casual Denim Shirt</h5>
+
+                        <a href="/detail/:id" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Read more
+                            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
