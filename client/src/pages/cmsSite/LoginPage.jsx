d19dd6c9cf241c0fdab6a31aa419857a9f0243c8
Reinawan <admin@reinawan.com>
2024-01-10 18:04:08 +0700

    feat: Login

--- client/src/pages/cmsSite/LoginPage.jsx
+++ client/src/pages/cmsSite/LoginPage.jsx
@@ -1,4 +1,51 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+
 export default function LoginPage() {
+
+    const [input, setInput] = useState({
+        email: "",
+        password: "",
+    });
+
+    const navigate = useNavigate();
+
+    const handleChangeInput = (e) => {
+        const { name, value } = e.target;
+        setInput({
+            ...input,
+            [name]: value,
+        });
+    };
+
+    const handleSubmitForm = async (e) => {
+        e.preventDefault()
+        try {
+          const {data} = await axios({
+            method: "post",
+            // url: "https://brandedthings.reinawan.fun/pub/products/login",
+            url: "http://localhost:3000/login",
+            // url: "http://3.0.97.244/",
+            data: input,
+        }); 
+
+        localStorage.access_token = data.access_token;
+
+        navigate("/listproducts");
+
+        console.log(data);
+        } catch (error) {
+            console.log(error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: error.response.data.message,
+            })
+        }
+    }
+
     return (
         <>
             <div id="login-modal" tabIndex="-1" aria-hidden="true" className="flex items-center justify-center fixed top-0 right-0 bottom-0 left-0 z-50">
@@ -8,9 +55,9 @@ export default function LoginPage() {
 
                         <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                             <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                                <b> Sign in to our platform </b>
+                                <b> Sign in to CMS site </b>
                             </h3>
-                            <a href="/"
+                            <Link to={"/"}
                                 type="button"
                                 className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
                             >
@@ -18,15 +65,15 @@ export default function LoginPage() {
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                                 </svg>
                                 <span className="sr-only">Close </span>
-                            </a>
+                            </Link>
                         </div>
 
                         <div className="p-4 md:p-5">
-                            <form className="space-y-4" action="#">
+                            <form onSubmit={handleSubmitForm} className="space-y-4" action="#">
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                                     <input
-
+                                        onChange={handleChangeInput}
                                         type="email"
                                         name="email"
                                         id="email"
@@ -38,7 +85,7 @@ export default function LoginPage() {
                                 <div>
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
                                     <input
-
+                                        onChange={handleChangeInput}
                                         type="password"
                                         name="password"
                                         id="password"
@@ -48,12 +95,12 @@ export default function LoginPage() {
                                     />
                                 </div>
 
-                                <a href="/listproducts"
+                                <button
                                     type="submit"
                                     className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                 >
                                     Login to your account
-                                </a>
+                                </button>
 
                             </form>
                         </div>
